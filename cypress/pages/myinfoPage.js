1a1a96d817982336610ddb328d976cc91df260ac
Andre Goncalves <andrearaujomti@gmail.com>
2024-09-01 22:38:13 -0300

    Created functions:  fillPersonalDetails, fillEmployee and fillStatus, deleted some functions in myinfoPage

--- cypress/pages/myinfoPage.js
+++ cypress/pages/myinfoPage.js
@@ -18,49 +18,31 @@ class MyInfoPage {
         }
         return selectors
     }
-    typeNameField() {
-        cy.get(this.selectorList().firstNameField).clear().type('First')
-        cy.get(this.selectorList().lastNameField).clear().type('Last')
+    fillPersonalDetails(firstName, lastName) {
+        cy.get(this.selectorList().firstNameField).clear().type(firstName)
+        cy.get(this.selectorList().lastNameField).clear().type(lastName)
     }
-    typeEmployeeField() {
-        cy.get(this.selectorList().genericField).eq(3).clear().type('Employee')
-    }
-    typeIDField() {
-        cy.get(this.selectorList().genericField).eq(4).clear().type('OtherID')
-    }
-    typeDLNField() {
-        cy.get(this.selectorList().genericField).eq(5).clear().type('DLN')
-    }
-    typeLEDField() {
-        cy.get(this.selectorList().dateCalendarField).eq(0).type('2025-15-07')
+    fillEmployee(employeeID, otherID, driveLicenseNumber, dataCalendar) {
+        cy.get(this.selectorList().genericField).eq(3).clear().type(employeeID)
+        cy.get(this.selectorList().genericField).eq(4).clear().type(otherID)
+        cy.get(this.selectorList().genericField).eq(5).clear().type(driveLicenseNumber)
+        cy.get(this.selectorList().dateCalendarField).eq(0).type(dataCalendar)
         cy.get(this.selectorList().calendarCloseButton).click()
     }
-    nationalityChoose() {
+    fillStatus(birthDate,testField) {
         cy.get(this.selectorList().dataSelectors).eq(0).click()
         cy.get(this.selectorList().dataNationalityChoose).click()
-    }
-    maritialChoose() {
         cy.get(this.selectorList().dataSelectors).eq(1).click()
         cy.get(this.selectorList().dataMaritialChoose).click()
-    }
-    typeDateOfBirth() {
-        cy.get(this.selectorList().dateCalendarField).eq(1).type('2000-01-12')
+        cy.get(this.selectorList().dateCalendarField).eq(1).type(birthDate)
         cy.get(this.selectorList().calendarCloseButton).click()
-    }
-    genderChoose() {
         cy.get(this.selectorList().dataGenderField).eq(0).click()
         cy.get(this.selectorList().dataSelectors).eq(2).click()
-    }
-    bloodTypeChoose() {
         cy.get(this.selectorList().dataBloodTypeField).click()
-    }
-    typeTestField() {
-        cy.get(this.selectorList().testFieldText).clear().type('123')
+        cy.get(this.selectorList().testFieldText).clear().type(testField)
     }
     submitSaveInfoButton() {
         cy.get(this.selectorList().submitMyInfoButton).eq(0).click()
-    }
-    validationSavedInfos() {
         cy.get(this.selectorList().validationToastContainer)
     }
 }
